996b0e3c40db09b50837afac09ee519488169f8c
kebi-gizachew <kebisel21@gmail.com>
2025-09-14 00:34:19 +0300

    Create Concatenation of Array.py

---
+++ Concatenation of Array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        if not nums:
+            return nums
+        k=nums+nums
+        return k
+        
